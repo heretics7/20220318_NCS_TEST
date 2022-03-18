1462105e0336941b30e8618f3fb67d7b48e2346a
heretics7 <heretics7@naver.com>
2022-03-18 11:14:29 +0900

    노드 라우팅 세팅

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express')
 var app = express();
 
+var getsend = require('./routes/getsend')
+var postsend = require('./routes/postsend')
+var sqlsend = require('./routes/sqlsend')
+
+app.use('/get', getsend)
+app.use('/post', postsend)
+app.use('/sql', sqlsend)
+
 app.set('port', 8080)
 app.get('/', (req, res) => {
     res.send('node setting')

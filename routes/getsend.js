b4ec87f581f114de05717792bce56dbc554d4b5d
heretics7 <heretics7@naver.com>
2022-03-18 12:12:54 +0900

    AWS SQL 연결 셋팅

--- routes/getsend.js
+++ routes/getsend.js
@@ -1,8 +1,28 @@
 var express = require('express');
+var mysql = require('mysql');
+var dbconfig = require('../db/config.js')
 var router = express.Router();
+var connection = mysql.createConnection(dbconfig)
 
 router.get('/', (req, res) => {
-    res.send('/get이라는 요청의 응답')
+    res.send('/get이라는 요청의 응답');
+})
+router.get('/getjson', (req, res) => {
+    res.send({'url':'/get/getjson'});
+})
+router.post('/post', (req, res) => {
+    res.send({
+        'url':'/get/post',
+        'title':'/getsend.js내의 post라우팅'
+    });
+})
+router.get('/awssql', (req, res) => {
+    connection.query('SELECT * FROM contact',(error, result) => {
+            if(error) throw error;
+            console.log('해당스키마안의 테이블 내용 : ', result)
+            res.send(result)
+        }
+    )
 })
 
 module.exports = router;

eb3030dcc2b6616420c4ef367f5d11dbb551c955
heretics7 <84853122+heretics7@users.noreply.github.com>
2022-03-18 16:42:16 +0900

    db 삭제

--- db/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host : "rds-demo-mysql.c92uyiveytek.us-east-1.rds.amazonaws.com",
-    user : "admin",
-    password : "Ksi10203040!",
-    port : '3306',
-    database : "reactinterview"
-}
